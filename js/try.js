89fad1ac0c5574dfb23dc924a4572cc3db16656b
Jesús Orihuela <jesus.orihuela@anahuac.mx>
2024-03-28 00:18:23 -0600

    try 2.3

--- js/try.js
+++ js/try.js
@@ -1,11 +1,13 @@
 let currentTheme = ''; // Variable global para almacenar el tema actual
 
 function setVariables(theme) {
+    console.log("setVariables");
     currentTheme = theme; // Asigna el tema actual
     loadPhrases(`json/${currentTheme}.json`); // Carga las frases basadas en el tema actual
 }
 
 function loadPhrases(jsonFile) {
+    console.log("loadPhrases");
     fetch(jsonFile)
         .then(response => response.json())
         .then(data => {
@@ -15,6 +17,7 @@ function loadPhrases(jsonFile) {
 }
 
 function displayRandomPhraseAndTranslation(phrases, translations) {
+    console.log("displayRandomPhraseAndTranslation");
     const phrasesContainer = document.getElementById('Phrase');
     const translationsContainer = document.getElementById('Traduction');
     const randomIndex = Math.floor(Math.random() * phrases.length);
