30f404d850c44ac2ba00e66356735fa543d3c534
Jesús Orihuela <jesus.orihuela@anahuac.mx>
2024-03-28 00:27:56 -0600

    try 3.1

--- js/try.js
+++ js/try.js
@@ -1,21 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM cargado');
     // Obtener la sección del almacenamiento local
     var section = localStorage.getItem('section');
 
     // Si hay una sección guardada, cargar las frases y traducciones
     if (section) {
         loadPhrases(`json/${section}.json`);
+        console.log('Sección cargada:', section);
     }
 });
 
 function setVariables(section) {
+    console.log('Sección seleccionada:', section);
     // Guardar la sección en el almacenamiento local
     localStorage.setItem('section', section);
     // Cargar las frases y traducciones para la sección seleccionada
-    loadPhrases(`json/${section}.json`);
+    loadPhrases(`../json/${section}.json`);
 }
 
 function loadPhrases(jsonFile) {
+    console.log('Cargando frases y traducciones:', jsonFile);
     fetch(jsonFile)
         .then(response => response.json())
         .then(data => {
@@ -25,6 +29,7 @@ function loadPhrases(jsonFile) {
 }
 
 function displayRandomPhraseAndTranslation(phrases, translations) {
+    console.log('Mostrando frase y traducción aleatoria');
     const phrasesContainer = document.getElementById('Phrase');
     const translationsContainer = document.getElementById('Traduction');
     const randomIndex = Math.floor(Math.random() * phrases.length);
