43aa6da81186472d676efbba92f414a2f17b280e
Jesús Orihuela <jesus.orihuela@anahuac.mx>
2024-03-28 00:07:53 -0600

    try 2.0

--- js/try.js
+++ js/try.js
@@ -9,17 +9,15 @@ function loadPhrases(jsonFile) {
     fetch(jsonFile)
         .then(response => response.json())
         .then(data => {
-            displayPhrases(data.phrases);
+            displayRandomPhraseAndTranslation(data.phrases, data.traductions);
         })
         .catch(error => console.error('Error:', error));
 }
 
-function displayPhrases(phrases) {
-    const phrasesContainer = document.getElementById('phrasesContainer');
-    phrasesContainer.innerHTML = ''; // Limpiar el contenedor antes de mostrar nuevas frases
-    phrases.forEach(phrase => {
-        const p = document.createElement('p');
-        p.textContent = phrase;
-        phrasesContainer.appendChild(p);
-    });
+function displayRandomPhraseAndTranslation(phrases, translations) {
+    const phrasesContainer = document.getElementById('Phrase');
+    const translationsContainer = document.getElementById('Traduction');
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    phrasesContainer.textContent = phrases[randomIndex];
+    translationsContainer.textContent = translations[randomIndex];
 }
