265044ddba535a12213da5aea2c0fbd85a0fdc8a
Jesús Orihuela <jesus.orihuela@anahuac.mx>
2024-03-28 10:54:24 -0600

    try 8.3

--- js/tryIndex.js
+++ js/tryIndex.js
@@ -1,3 +1,4 @@
+//Función que establece las variables de tema y redirige al usuario a tryPractice.html
 function setVariables(theme) {
     localStorage.setItem('currentTheme', theme); // Guarda el tema actual en el almacenamiento local
     window.location.href = 'tryPractice.html'; // Redirige al usuario a tryPractice.html
