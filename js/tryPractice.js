ae3cc0f4e060c46509fa4165edd473dd77f6a7b3
Jesús Orihuela <jesus.orihuela@anahuac.mx>
2024-03-28 19:23:17 -0600

    going back to where it worked

--- js/tryPractice.js
+++ js/tryPractice.js
@@ -1,27 +1,10 @@
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Variables
-const listenButton = document.getElementById('listenButton');
-const phraseElement = document.getElementById('Phrase');
-const message = document.getElementById('recognizedText');
-const tryAgainButton = document.getElementById('tryAgainButton');
-const tryAnotherButton = document.getElementById('tryAnotherButton');
-let understood = false;
-let microphone = false;
-
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Función para 
 document.addEventListener('DOMContentLoaded', function() {
-    // Crea una nueva instancia de JSConfetti
-    const jsConfetti = new JSConfetti();
-    // Obtiene el tema actual del almacenamiento local
     const currentTheme = localStorage.getItem('currentTheme'); // Obtiene el tema actual del almacenamiento local
     if (currentTheme) {
         loadPhrases(`json/${currentTheme}.json`); // Carga las frases basadas en el tema actual
     }
 });
 
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Función para 
 function loadPhrases(jsonFile) {
     fetch(jsonFile)
         .then(response => response.json())
@@ -32,8 +15,6 @@ function loadPhrases(jsonFile) {
         .catch(error => console.error('Error:', error));
 }
 
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Función para
 function getRandomPhraseAndTranslation(phrases, translations) {
     const randomIndex = Math.floor(Math.random() * phrases.length);
     return {
@@ -42,14 +23,10 @@ function getRandomPhraseAndTranslation(phrases, translations) {
     };
 }
 
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Función para
 function displayPhraseAndTranslation(phrase, translation) {
+    const phraseElement = document.getElementById('Phrase');
     phraseElement.textContent = phrase; // Muestra la frase en el contenedor 'Phrase'
 
+    const translationElement = document.getElementById('Traduction');
     translationElement.textContent = translation; // Muestra la traducción en el contenedor 'Traduction'
 }
-
-// ----------------------------------------------------------------------------------------------------------------------------------
-// Agregar el evento de clic al botón listenButton
-
